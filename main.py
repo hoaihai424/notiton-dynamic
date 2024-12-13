d66d94f171f112ca42cc850366374ddc38350e77
Pham Le Hoai Hai <hoaihai13579@gmail.com>
2024-12-13 16:22:29 +0700

    finish retrieve + initiate create, update, delete, helper, main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-import requests
-import json
 from fastapi import FastAPI, Depends, HTTPException
-import pandas as pd
-
-NOTION_API_KEY = "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"
-NOTION_URL = "https://api.notion.com/v1/databases"
-DATABASE_ID = "1532ec8473e9803ba028c913c45aa06b"
-
-headers = {
-    "Authorization": "Bearer " + NOTION_API_KEY,
-    "Content-Type": "application/json", 
-    "Notion-Version": "2022-06-28", 
-}
 
 app = FastAPI()
 
-@app.get("/customers/{customer_id}")
-async def get_customer(customer_id: str, num_pages: int = None):
-    url = f"{NOTION_URL}/{DATABASE_ID}/query"
+@app.get("/customers")
+async def read_customer():
+    pass
 
-    page_size = 100 if num_pages is None else num_pages
-    payload = {"page_size": page_size}
+@app.get("/customers/{customer_id}")    
+async def read_customer(customer_id: int):
+    pass
 
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    data = response.json()
-    results = data["results"]
+@app.get("/customers/{customer_id}/database")
+async def read_customer_database(customer_id: int):
+    pass
+
+@app.post("/customers")
+async def create_customer():
+    pass
+
+@app.put("/customers/{customer_id}")
+async def update_customer(customer_id: int):
+    pass
+
